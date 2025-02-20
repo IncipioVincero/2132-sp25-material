3a7dfcbaf89aa3cd3900d45ca570d6471c6179ba
Daniel Bauer <bauer@cs.columbia.edu>
2025-02-20 08:39:45 -0500

    updates from feb 19 class

--- lectures/lecture06/linkedlist.py
+++ lectures/lecture06/linkedlist.py
@@ -101,6 +101,20 @@ class DoubleNode:
     self.prev = prev
     self.next = next
     
+
+class LinkedListIterator: 
+
+  def __init__(self, current):
+    self.current = current
+
+  def next(self):
+    if self.current.next == None: 
+      raise StopIteration
+    element = self.current.element
+    self.current = current.next
+    return element    
+
+
 class DoubleLinkedList(List): 
 
   def __init__(self): 
@@ -109,14 +123,25 @@ class DoubleLinkedList(List):
     self._head.next = self._tail
     self.len = 0
 
+  def iterator(self): 
+    return LinkedListIterator(self._head.next)
+
+
   def get_node(self, k):
-    
+   
     if k == -1: 
-      return self.head    
- 
-    current = self._head.next
-    for i in range(k):
-      current = current.next
+      return self._head    
+
+    if k <= self.len // 2:   # k in first half
+      current = self._head.next
+      for i in range(k):
+        current = current.next
+    else:                   # k in second half    
+      current = self._tail.prev
+      for i in range(self.len - k -1):
+        current = current.prev
+      
+
     return current
 
   def get(self, k):  #retrieve element at index k
@@ -126,19 +151,27 @@ class DoubleLinkedList(List):
     if k > self.len: 
       raise IndexError("invalid list index") 
 
-    current = self.get_node(k-1)
-    node = None(x, None, None) 
-    node.next = current.next 
-    current.next = node
+    pred = self.get_node(k-1)
+    node = DoubleNode(x, None, None) 
+    node.next = pred.next 
+    pred.next = node
     node.next.prev = node
-    node.prev = curr
+    node.prev = pred 
     self.len += 1
 
   def remove(self, k):  #remove element at index k
       if k >= self.len:
         raise IndexError("invalid list index")
 
-      #pass TODO
+      node = self.get_node(k)
+      pred = node.prev
+      succ = node.next
+
+      pred.next = succ
+      succ.prev = pred
+      node.next = None
+      node.prev = None
+      self.len -= 1
 
   def remove_first(self): #aka pop
     self.remove(0)
@@ -158,13 +191,82 @@ class DoubleLinkedList(List):
     if self.len == 0:
       return "[ ]"
 
-    current = self._head
+    current = self._head.next
     result = "["
-    while current.next != None: #for i in range(self.len):
+    while current.next != self._tail: #for i in range(self.len):
       result = result + str(current.element) + " "
       current = current.next
     result = result + str(current.element) + "]"
     return result 
+   
+class Deque:
 
+    def __init__(self): 
+        self._list = DoubleLinkedList()
+
+    def add_first(self, e):
+        'Add element e to the front of the double-ended queue'
+         self._list.insert(0, e)
+
+    def add_last(self, e):
+        'Add element e to the end of the double-ended queue'
+        self._list.append(e)
+
+    # Renamed deque operation
+    def remove_first(self):
+        'Remove the first element from the double-ended queue'
+        element = self.get(0) 
+        self._list.remove(0)
+        return element
+
+    def remove_last(self):
+        'Remove the last element from the double-ended queue'
+        element = self.get(self._list.len-1)
+        self._list.remove(self._list.len-1)
+        return element
+
+    # Auxiliary operations
     
+    def first(self):
+        'Return a reference to the first element in the double-ended queue'
+        return self.get(0)
+
+    def last(self):
+        'Return a reference to the last element in the double-ended queue'
+        return self.get(self._list.len-1)
+
+    def is_empty(self):
+        return self._list.len == 0
+
+    def __len__(self):
+        return self._list.len 
+
+
+
+li = DoubleLinkedList()
+li.append(1)
+li.append(2)
+li.append(3)
+
+
+
+
+# Runtime? O(N^2) with a linked list,  O(N) with array list
+for i in range(li.len): 
+  print(li.get(i))
+
+# With LinkedListIterator: O(N) on a linked list
+for x in li: 
+  print(x)
+
+iter = li.iterator()
+try:
+  while True: 
+    x = iter.next()
+except StopIteration: 
+   pass
+
+
+
+
 
